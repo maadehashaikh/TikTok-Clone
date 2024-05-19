66e249eaee00305f2c472cbdb0c1f2dd4ef9deed
Maadeha shaikh <maadeham@gmail.com>
2024-05-19 22:56:05 +0500

    again new commit

--- tiktokclone/src/FooterLeft.jsx
+++ tiktokclone/src/FooterLeft.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import MusicNote from './icons/MusicNote'
 import './FooterLeft.css'
-import MusicNoteIcon from './icons/MusicNoteIcon';
 
 export default function VideoFooter(props) {
+	const { channel, description, song } = props
 	// write code here
-  const {channel, description, song } = props;	
-	<div className="footer-left">
-		<div className="text">
-			<h3>@{channel}</h3>
-			<p>{description}</p>
-			<div className="ticker">
-				 <MusicNoteIcon style={{ width: '30px' }} />
-				<marquee direction="left" scrollamount="2">
-					{song}
-				</marquee>
-			</div>
+	return <div className="footer-left">
+	<div className="text">
+		<h3>@{channel}</h3>
+		<p>{description}</p>
+		<div className="ticker">
+			<MusicNote style={{ width: '30px' }} />
+			<marquee direction="left" scrollamount="2">
+				{song}
+			</marquee>
 		</div>
 	</div>
-
-	return null
+</div>
 }
+
+console.log("NO COMMIT TODAY ");
